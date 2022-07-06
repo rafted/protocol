b0e09d5823727668790c039b4a47d344fc305031
w <pcranaway@tuta.io>
2022-07-06 23:56:24 +0200

    remove ktlint because it's geting on my nerves..

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("java")
     kotlin("jvm") version "1.7.0"
-    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+//    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
 }
 
 allprojects {
@@ -18,7 +18,7 @@ allprojects {
 }
 
 subprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+//    apply(plugin = "org.jlleitschuh.gradle.ktlint")
     apply(plugin = "java")
 
     dependencies {
