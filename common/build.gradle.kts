8c1c00d1fd57d5534fa41e7789cd68d76530c1ed
w <pcranaway@tuta.io>
2022-07-06 15:55:30 +0200

    add adventure dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
 
 dependencies {
     implementation("io.netty:netty-all:4.1.78.Final")
+    implementation("net.kyori:adventure-api:4.11.0")
+    implementation("net.kyori:adventure-text-serializer-gson:4.11.0")
 }
